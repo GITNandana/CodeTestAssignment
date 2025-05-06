aef0ab56f2081ee90ed23b8ee9a81cd8eba719c1
GITNandana <srinandana.sri6@gmail.com>
2025-05-05 22:03:29 -0700

    Missed Json Conversion

--- ReqUserService.Domain/Models/User.cs
+++ ReqUserService.Domain/Models/User.cs
@@ -4,8 +4,22 @@
     {
         public int Id { get; set; }
         public string Email { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string Avatar { get; set; }
+    }
+
+    public class JsonData
+    {
+        public User Data { get; set; }
+        public Support Support { get; set; }
+    }
+
+    
+    public class Support
+    {
+        public string Url { get; set; }
+        public string Text { get; set; }
     }
 
 }
